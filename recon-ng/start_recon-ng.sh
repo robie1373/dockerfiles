3d9ce7a2b1832baf9e3e776602b5d9a09e1fb519
Robie Lutsey <rlutsey@Informations-MacBook-Pro.local>
2015-12-29 15:50:56 -0500

    nmap and recon working with scripted persistance.

---
+++ recon-ng/start_recon-ng.sh
@@ -0,0 +1,15 @@
+scriptname=recon-ng
+destdir=/recon-ng
+
+command="docker run -t -i -v $(pwd)/perm:$destdir/perm $scriptname"
+
+if [[ ! -d perm ]]
+  then  cd ~/dockerfiles/$scriptname/ 
+fi
+
+if [[ -d perm ]]
+  then  $command
+else echo "Can't find perm. It lives next to the Dockerfile for this container. Try to cd there and try again."
+
+fi
+
