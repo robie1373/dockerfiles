3a33b6877313442c487b232ec828fced22de6898
Robie <robie1373@gmail.com>
2016-01-21 23:54:01 +0000

    made jekyll volume independant

--- jekyll/start_jekyll.sh
+++ jekyll/start_jekyll.sh
@@ -1,16 +1,6 @@
 scriptname=jekyll
-destdir=/blog
-localdir=~/Dropbox/data/writings/blog
 
-command="docker run -p 4000:4000 --rm -t -i -v $localdir:$destdir/source $scriptname-image"
+command="docker run -p 4000:4000 --rm -ti $scriptname-image"
 
-if [[ ! -d $localdir ]]
-  then  cd ~/dockerfiles/$scriptname/ 
-fi
-
-if [[ -d $localdir ]]
-  then  $command
-else echo "Can't find $localdir. Fix this and try again."
-
-fi
+$command
 
