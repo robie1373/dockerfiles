0bf25eaa60dcec6b205542413f36204750cd1a8a
Robie <robie1373@gmail.com>
2016-11-24 15:18:09 +0000

    cleaning up

--- recon-ng/start_recon-ng.sh
+++ recon-ng/start_recon-ng.sh
@@ -1,15 +1,12 @@
 scriptname=recon-ng
 destdir=/recon-ng
 
-command="docker run -t -i -v $(pwd)/perm:$destdir/perm $scriptname"
+command="docker run -t -i -v $(pwd)/perm:$destdir/perm $scriptname-image"
 
 if [[ ! -d perm ]]
   then  cd ~/dockerfiles/$scriptname/ 
 fi
 
-if [[ -d perm ]]
-  then  $command
-else echo "Can't find perm. It lives next to the Dockerfile for this container. Try to cd there and try again."
+  $command
 
-fi
 
